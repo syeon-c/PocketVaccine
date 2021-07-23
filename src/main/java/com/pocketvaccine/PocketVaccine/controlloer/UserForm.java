23262f7ab717bb12f36f10fd0375fedeaf692ba9
최서연 <gkdltjdus@gmail.com>
2021-07-23 21:08:25 +0900

    #gradle 수정2

--- src/main/java/com/pocketvaccine/PocketVaccine/controlloer/UserForm.java
+++ src/main/java/com/pocketvaccine/PocketVaccine/controlloer/UserForm.java
@@ -1,31 +1,13 @@
 package com.pocketvaccine.PocketVaccine.controlloer;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class UserForm {
+
     private String id;
     private String name;
     private String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
