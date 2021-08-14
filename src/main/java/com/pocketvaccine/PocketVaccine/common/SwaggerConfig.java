153429c209d766f4ee268475f03e7cd5463f7447
최서연 <gkdltjdus@gmail.com>
2021-08-15 01:46:56 +0900

    swagger 보류

--- src/main/java/com/pocketvaccine/PocketVaccine/common/SwaggerConfig.java
+++ src/main/java/com/pocketvaccine/PocketVaccine/common/SwaggerConfig.java
@@ -1,34 +1,37 @@
-package com.pocketvaccine.PocketVaccine.common;
+//package com.pocketvaccine.PocketVaccine.common;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.hateoas.client.LinkDiscoverer;
+//import org.springframework.hateoas.client.LinkDiscoverers;
+//import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+//import org.springframework.plugin.core.SimplePluginRegistry;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//import java.util.ArrayList;
+//import java.util.List;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-
-    @Bean
-    public Docket restAPI() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.pocketvaccine"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("PocketVaccine Spring Boot REST API")
-                .version("1.0.0")
-                .description("백신 접종 후기 및 정보 공유 커뮤니티 PocketVaccine swagger api 입니다.")
-                .build();
-    }
-}
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerConfig {
+//    @Bean
+//    public Docket Docket() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.pocketvaccine"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    @Bean
+//    public LinkDiscoverers discoverers() {
+//        List<LinkDiscoverer> plugins = new ArrayList<>();
+//        plugins.add(new CollectionJsonLinkDiscoverer());
+//        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+//    }
+//}
