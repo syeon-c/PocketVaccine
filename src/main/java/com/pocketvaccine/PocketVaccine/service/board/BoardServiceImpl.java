8d2f82818a57d95796350082a38912c8a0a2f706
최서연 <gkdltjdus@gmail.com>
2021-08-16 14:39:15 +0900

    likes 수정

--- src/main/java/com/pocketvaccine/PocketVaccine/service/board/BoardServiceImpl.java
+++ src/main/java/com/pocketvaccine/PocketVaccine/service/board/BoardServiceImpl.java
@@ -41,7 +41,7 @@ public class BoardServiceImpl implements BoardService {
                 .content(boardDto.getContent())
                 .ageRange(boardDto.getAgeRange())
                 .createdAt(boardDto.getCreatedAt())
-                .likes(0)
+                .likes(boardDto.getLikes())
                 .vaccineDose(boardDto.getVaccineDose())
                 .vaccineType(boardDto.getVaccineType())
                 .musclePain(boardDto.isMusclePain())
